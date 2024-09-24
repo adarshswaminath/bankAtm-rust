3e293134746e723f5c6e95bb60e141176b1ca41a
adarshswaminath <adarshswaminath7@gmail.com>
2024-09-24 23:04:18 +0530

    feat: simple banner created

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use terminal_banner::Banner;
 
 fn main() {
     banner("Core Bank Of Rust");
+    let option = menu_list();
+    println!("Option Selected: {}", option);
 }
 
 fn banner(title: &str) {
@@ -11,6 +13,21 @@ fn banner(title: &str) {
         .text("Welcome to the Core Bank Of Rust".into())
         .render();
     println!("{}", banner)
+}
+
+
+fn menu_list() -> u32 {
+    let mut user_option = String::new();
+    println!("<============ Menu List ===========>");
+    println!("1. Create Account");
+    println!("2. Deposit");
+    println!("3. Withdraw");
+    println!("4. Balance Inquiry");
+    println!("5. Exit");
+    println!("Please Select a Option: ");
+    std::io::stdin().read_line(&mut user_option).expect("Err -> Cannot read the input");
+    let user_option: u32 = user_option.trim().parse().expect("Err -> Not a number");
+    return user_option;
 }
 
 fn clear_terminal() {
