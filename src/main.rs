afc9a52e030bc6ee8396798c3afda183d7e5fe36
adarshswaminath <adarshswaminath7@gmail.com>
2024-09-25 00:10:06 +0530

    feat( :tada: ): ATM base functionality completed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 use terminal_banner::Banner;
 
 fn main() {
-    let is_verified = verify_user();
+    let is_verified = verify_user(); // verification status
+    let mut balance = 1000; // user account balance amount
     if is_verified {
         banner("Core Bank Of Rust");
         let option = menu_list();
-        println!("Option Selected: {}", option);
+        
+        if option == 1 {
+            // ? deposit amount
+            let amount_deposite = user_input_amount();
+            balance = balance + amount_deposite;
+            println!("Your Balance ${}", balance);
+
+        } else if option == 2 {
+            // ? withdraw amount
+            let amount_withdraw = user_input_amount();
+            // ! Insufficient Balance
+            if amount_withdraw > balance {
+                println!("Insufficient Balance");
+            }
+            // ? withdraw amount and update balance
+            balance = balance - amount_withdraw;
+            println!("Your Balance ${}", balance);
+        } else if option == 3 {
+            println!("Your Balance ${}", balance);
+        } else {
+            println!("Thank you for using Core Bank Of Rust");
+        }
     } else {
         println!("Access Denied");
     }
 }
 
-
-
+fn user_input_amount() -> u32 {
+    let mut user_input = String::new();
+    println!("Please Enter Amount: ");
+    std::io::stdin()
+        .read_line(&mut user_input)
+        .expect("Err -> Cannot read this line ");
+    let user_input: u32 = user_input
+        .trim()
+        .parse()
+        .expect("Err -> Error in convertion");
+    return user_input;
+}
 /*
 * @description: Verifies the user
 * @return: boolean result of the verification
@@ -22,9 +54,14 @@ fn verify_user() -> bool {
     let password = 1234;
     let mut user_password_input = String::new();
     println!("Please Enter Your Secret PIN: ");
-    std::io::stdin().read_line(&mut user_password_input).expect("Err -> Cannot read the input");
-    let user_password_input: u32 = user_password_input.trim().parse().expect("Err -> Not a number");
-    return  user_password_input == password;
+    std::io::stdin()
+        .read_line(&mut user_password_input)
+        .expect("Err -> Cannot read the input");
+    let user_password_input: u32 = user_password_input
+        .trim()
+        .parse()
+        .expect("Err -> Not a number");
+    return user_password_input == password;
 }
 
 /*
@@ -40,7 +77,6 @@ fn banner(title: &str) {
     println!("{}", banner)
 }
 
-
 /*
 * @description: Prints the menu list
 * @return: The selected option integer
@@ -48,13 +84,14 @@ fn banner(title: &str) {
 fn menu_list() -> u32 {
     let mut user_option = String::new();
     println!("<============ Menu List ===========>");
-    println!("1. Create Account");
-    println!("2. Deposit");
-    println!("3. Withdraw");
-    println!("4. Balance Inquiry");
-    println!("5. Exit");
+    println!("1. Deposit");
+    println!("2. Withdraw");
+    println!("3. Balance Inquiry");
+    println!("4. Exit");
     println!("Please Select a Option: ");
-    std::io::stdin().read_line(&mut user_option).expect("Err -> Cannot read the input");
+    std::io::stdin()
+        .read_line(&mut user_option)
+        .expect("Err -> Cannot read the input");
     let user_option: u32 = user_option.trim().parse().expect("Err -> Not a number");
     return user_option;
 }
