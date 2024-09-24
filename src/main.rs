97adf0dc56bd828c6ef6d99d42cde922c8507e67
adarshswaminath <adarshswaminath7@gmail.com>
2024-09-24 23:44:50 +0530

    feat: user pin verification added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use terminal_banner::Banner;
 
 fn main() {
-    banner("Core Bank Of Rust");
-    let option = menu_list();
-    println!("Option Selected: {}", option);
+    let is_verified = verify_user();
+    if is_verified {
+        banner("Core Bank Of Rust");
+        let option = menu_list();
+        println!("Option Selected: {}", option);
+    } else {
+        println!("Access Denied");
+    }
 }
 
+
+
+/*
+* @description: Verifies the user
+* @return: boolean result of the verification
+*/
+fn verify_user() -> bool {
+    clear_terminal();
+    let password = 1234;
+    let mut user_password_input = String::new();
+    println!("Please Enter Your Secret PIN: ");
+    std::io::stdin().read_line(&mut user_password_input).expect("Err -> Cannot read the input");
+    let user_password_input: u32 = user_password_input.trim().parse().expect("Err -> Not a number");
+    return  user_password_input == password;
+}
+
+/*
+* @description: Prints the banner
+* @param title: The title of the banner
+*/
 fn banner(title: &str) {
     clear_terminal();
     let banner = Banner::new()
@@ -16,6 +41,10 @@ fn banner(title: &str) {
 }
 
 
+/*
+* @description: Prints the menu list
+* @return: The selected option integer
+*/
 fn menu_list() -> u32 {
     let mut user_option = String::new();
     println!("<============ Menu List ===========>");
